ca111a20821bc5a0ff4e99127ab3bcd64173f195
bowlofruit <ser150448@gmail.com>
2024-01-17 02:18:00 +0200

    Initial commit

---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace Notepad
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public void Open_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			if (openFileDialog.ShowDialog() == true)
+			{
+				string filePath = openFileDialog.FileName;
+				txtEditor.Text = File.ReadAllText(filePath);
+			}
+		}
+
+		public void Save_Click(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			if (saveFileDialog.ShowDialog() == true)
+			{
+				string filePath = saveFileDialog.FileName;
+				File.WriteAllText(filePath, txtEditor.Text);
+			}
+		}
+
+		private void Exit_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
---
+++ NotepadTests/MainWindowTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Notepad;
+using System.IO;
+using System.Threading;
+using System.Windows.Controls;
+
+namespace WPFNotepad.Tests
+{
+	[TestClass]
+	public class MainWindowTests
+	{
+		private MainWindow mainWindow;
+		private ManualResetEvent mainWindowInitializedEvent = new ManualResetEvent(false);
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			Thread thread = new Thread(() =>
+			{
+				mainWindow = new MainWindow();
+				mainWindowInitializedEvent.Set(); // Сигналізує, що головне вікно ініціалізовано
+				System.Windows.Threading.Dispatcher.Run();
+			});
+
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();
+
+			// Чекаємо, доки головне вікно буде повністю ініціалізовано
+			mainWindowInitializedEvent.WaitOne();
+		}
+
+		[TestMethod]
+		public void OpenFileTest()
+		{
+			string testFilePath = "test.txt";
+
+			mainWindow.Dispatcher.Invoke(() =>
+			{
+				File.WriteAllText(testFilePath, "Test content");
+
+			 	TextBox textBox = (TextBox)mainWindow.FindName("txtEditor");
+				Assert.IsNotNull(textBox);
+				mainWindow.Open_Click(null, null);
+				Assert.AreEqual("Test content", textBox.Text);
+			});
+		}
+
+		[TestMethod]
+		public void SaveFileTest()
+		{
+			string testFilePath = "test.txt";
+			
+			mainWindow.Dispatcher.Invoke(() =>
+			{
+				TextBox textBox = (TextBox)mainWindow.FindName("txtEditor");
+				Assert.IsNotNull(textBox);
+
+				textBox.Text = "Test content";
+				mainWindow.Save_Click(null, null);
+			});
+
+			Assert.AreEqual("Test content", File.ReadAllText(testFilePath));
+
+			File.Delete(testFilePath);
+		}
+	}
+}
